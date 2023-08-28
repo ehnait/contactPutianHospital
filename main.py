0ac492efedb8f3f50dd409670c84a6550f511b5b
ehnait <17611000331@163.com>
2023-08-28 15:58:55 +0800

    修复效率问题 #issues/2

--- main.py
+++ main.py
@@ -1,96 +1,93 @@
+import random
+import time
 from concurrent.futures import ThreadPoolExecutor
+
 from DrissionPage import ChromiumPage, ChromiumOptions
-import requests
 
 # http://g1879.gitee.io/drissionpagedocs/ChromiumPage/browser_options/
 co = ChromiumOptions()
-# 不加载图片、有界面模式、超时时间、自动获取端口
-co.set_no_imgs(True).set_headless(False).set_timeouts(implicit=2).auto_port(True)
+# 不加载图片、有界面模式、自动获取端口
+co.set_no_imgs(True).set_headless(False).auto_port(True)
 
-# 是否开启多线程多Tab标签模式
-IS_ENABLE_MULTI_TAB = False
 co.set_paths(browser_path=r'这里修改为您的浏览器可执行文件路径，可以在chrome浏览器的地址栏中输入：chrome://version 查看')
 tel_number = '手机号码'
 
 
-def check_url(url):
+def process_tab(page, url):
+    time.sleep(random.uniform(0, 3))  # 自己权衡是否需要延迟
+    """
+      处理网页标签
+        - 使用 page.new_tab(url) 创建新的标签页，返回标签页 ID（tid）
+        - 获取页面上指定 ID 的标签页（tab）对象
+        - 等待页面加载完成后再处理该标签页
+        - 检查再次是否存在类名为 "pc-icon-leave-tel" 的元素（离开电话图标）
+            - 若存在，则点击离开电话图标，并在 "leavetel-input" 元素中输入电话号码
+        - 检查类名为 "leavetel-callback" 的元素（回拨按钮）
+            - 若存在，则点击回拨按钮，并返回字典对象：{"state": True, "title": tab.title}
+      :param page: ChromiumPage对象，用于管理和操作浏览器标签页
+      :param url: 网页地址
+      :return: 字典，包含处理结果状态和标题信息
+      """
+
+    tab = None
     try:
-        response = requests.get(url)
-        response.raise_for_status()  # 检查响应状态码是否为200
-        # print("URL is accessible")
-        return True
-    except requests.exceptions.RequestException as e:
+        tid = page.new_tab(url)
+        tab = page.get_tab(tid)
+        tab.wait.load_start()
+
+        icon_leave_tel = tab.ele('@class:pc-icon-leave-tel')
+        if icon_leave_tel:
+            icon_leave_tel.click(by_js=True)
+            leavetel_input = tab.ele('@class:leavetel-input')
+            leavetel_input.input(tel_number)
+            callback = tab.ele('@class:leavetel-callback')
+            if callback:
+                callback.click(by_js=True)
+                return {"state": True, "title": tab.title}
+
+
+    except Exception as e:
         print(f"An error occurred: {e}")
-        return False
+    finally:
+        if tab:
+            page.close_tabs(tabs_or_ids=tab)
 
 
-def process_tab(page):
-    pass_mod_display = page.ele('@class:passMod_dialog-wrapper passMod_show')
-    if pass_mod_display:
-        page.refresh()
-        page.wait.load_complete()
+def iterate_api(file_path, workers=4):
+    """
+    迭代处理API
+      - 使用 ChromiumPage 创建浏览器页面对象（page）
+      - 读取文件中的所有 URL，并计算总长度（total_len）
+      - 使用 split_list 函数将 URL 列表拆分成多个子列表，每个子列表最大长度为4
+      - 初始化成功计数器 success_count
+      - 遍历每个子列表，使用线程池并发执行 process_tab 函数，并通过 map 获取结果
+      - 如果返回结果不为空，则获取状态和标题，并根据状态更新成功计数器和打印输出信息
+    :param file_path: API文件路径
 
-    captcha_close_display = page.ele('@class:imlp-component-captcha-close')
-    if captcha_close_display:
-        captcha_close_display.click()
-
-    leavetel_input = page.ele('@class:leavetel-input')
-    if not leavetel_input:
-        leave_tel = page.ele('@class:pc-icon-leave-tel')
-        if leave_tel:
-            leave_tel.click()
-            leavetel_input = page.ele('@class:leavetel-input')
-
-    if leavetel_input:
-        leavetel_input.input(tel_number)
-        callback = page.ele('@class:leavetel-callback')
-        if callback:
-            callback.click()
-
-
-def iterate_api(file_path):
+    """
     page = ChromiumPage(addr_driver_opts=co)
-
     with open(file_path, 'r', encoding='utf-8') as file:
         urls = file.readlines()
+        total_len = len(urls)
 
     success_count = 0
-    if IS_ENABLE_MULTI_TAB:
-        with ThreadPoolExecutor() as executor:
-            cur_index = 1
-            for tid in executor.map(page.new_tab, urls):
-                try:
-                    new_tab = page.get_tab(tid)
-                    new_tab.wait.load_start()
-                    print(f"序号{cur_index} , 标题:{new_tab.title}")
-                    process_tab(new_tab)
-                    success_count += 1
-                    print(f"成功数量, {success_count}/{len(urls)}")
-                except Exception as e:
-                    print(f"Exception：{str(e)}")
-                finally:
-                    cur_index += 1
-                    if new_tab:
-                        page.close_tabs(tabs_or_ids=new_tab)
 
-        page.quit()
-    else:
-        try:
-            for url in urls:
-                accessible = check_url(url)
-                if accessible:
-                    page.get(url)
-                    process_tab(page)
+    with ThreadPoolExecutor(max_workers=workers) as executor:
+        for result in executor.map(lambda url: process_tab(page, url), urls):
+            if result:
+                state = result["state"]
+                title = result["title"]
+                if state:
                     success_count += 1
-                    print(f"成功数量, {success_count}/{len(urls)}")
-        except Exception as e:
-            print(f"Exception：{str(e)}")
-        finally:
-            page.quit()
+                    print(f"成功数量, {success_count}/{total_len} ,标题:{title}")
 
 
 if __name__ == '__main__':
     if tel_number.isdigit():
+        start_time = time.time()
         iterate_api('api.txt')
+        end_time = time.time()
+        print(f"结束！总耗时: {end_time - start_time} seconds")
+
     else:
         print("请先输入手机号码")
